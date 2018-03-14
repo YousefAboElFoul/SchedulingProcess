82f90e7f8d96fc28a28f7ba14197ba72f2905ec0
Yazan Odeh <yazanodeh7@hotmail.com>
2018-03-14 17:48:46 -0400

    process queue

--- Lab3/Lab3/Scheduler.cpp
+++ Lab3/Lab3/Scheduler.cpp
@@ -1,24 +1,44 @@
 #include "Scheduler.h"
+#include "process.h"
+#include <iostream>
 
-
+using namespace std;
 
 Scheduler::Scheduler()
 {
 }
 Scheduler::Scheduler(int,int,bool,bool)
 {
+
 }
-void Scheduler::setQueue1(){}
-int Scheduler::getQueue1(){}
+void Scheduler::setQueue1()
+{
+
+}
+process Scheduler::getQueue1()
+{
+}
 //--------------------------------------//
-void Scheduler::setQueue2(){}
-int Scheduler::getQueue2(){}
+void Scheduler::setQueue2()
+{
+}
+process Scheduler::getQueue2()
+{
+}
 //--------------------------------------//
-void Scheduler::setFlag1(){}
-bool Scheduler::getFlag1(){}
+void Scheduler::setFlag1()
+{
+}
+bool Scheduler::getFlag1()
+{
+}
 //--------------------------------------//
-void Scheduler::setFlag2(){}
-bool Scheduler::getFlag2(){}
+void Scheduler::setFlag2()
+{
+}
+bool Scheduler::getFlag2()
+{
+}
 
 
 
