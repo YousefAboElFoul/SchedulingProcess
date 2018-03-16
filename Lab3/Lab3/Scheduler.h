a6ef799bb2941e6014605ac680d069da80810372
Yazann7 <32470031+Yazann7@users.noreply.github.com>
2018-03-16 16:50:52 -0400

    Update Scheduler.h

--- Lab3/Lab3/Scheduler.h
+++ Lab3/Lab3/Scheduler.h
@@ -7,23 +7,21 @@ class Scheduler
 {
 public:
 	Scheduler();
-	Scheduler(process, process, bool, bool);
+	Scheduler(bool);
 	//------setters and getters-------------//
-	void setQueue1();
-	process *getQueue1();
+
+	process * getQueue1();
 	//--------------------------------------//
-	void setQueue2();
-	process*getQueue2();
+	
+	process * getQueue2();
+
 	//--------------------------------------//
-	void setFlag1();
-	bool getFlag1();
-	//--------------------------------------//
-	void setFlag2();
-	bool getFlag2();
+	void setFlag(bool);
+	bool getFlag();
 	//--------------------------------------//
 
-<<<<<<< HEAD
-=======
+	//--------------------------------------//
+
 	bool insertProcess(process * [], process * );
 	bool removeProcess(process * []);
 
@@ -33,9 +31,9 @@ public:
 
 	void updatePriority();
 
->>>>>>> master
 private:
-	process Queue1[10], Queue2[10];
-	bool Flag1,FLag2;
+	process * Queue1[140];
+	process * Queue2[140];
+	bool Flag;
 };
 
