1ecf49cb80941750986b4c768f4dd9489337dbdf
YousefAboElFoul <yousefaboelfoul@gmail.com>
2018-03-16 18:17:21 -0400

    source added

--- Lab3/Lab3/Source.cpp
+++ Lab3/Lab3/Source.cpp
@@ -1,24 +1,57 @@
-
+#include "process.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <sstream>
+#include <vector>
 
 //#include <pthread.h/LINUX>
-
+int size;
 using namespace std;
+
 int main() {
-	clock_t start;
-	start=clock();//setting 
+//	clock_t start;
+//	start=clock();//setting 
+	int size;
 	int my_num;
 	string line;
 	ifstream myfile;
+	int counter = 0;
+	vector<process> Process_size;
+	string PID;
+	int arrival_time,burst_time,priorty;
+
+
+	
+	
 	 myfile.open ("pbsinput.txt");
 
 	if (myfile.is_open())
 	{
 		while (getline(myfile, line))
 		{
+			if (counter == 0)
+			{
+				istringstream buffer(line);				//swtiches string into an intger
+				buffer >> size;
+				cout << "size is:" << size << endl;
+				Process_size.resize(size);
+				counter++;
+			}
+			else
+				myfile >> PID >> arrival_time >> burst_time >> priorty;
+				
+				
+					Process_size[counter-1].set_PID(PID);
+					Process_size[counter-1].set_arrival_time(arrival_time);
+					Process_size[counter - 1].set_burst_time(burst_time);
+					Process_size[counter - 1].set_priority(priorty);
+					counter++;
+
+
+				
+			
 			cout << line << '\n';
 		//num
 		}
@@ -26,7 +59,7 @@ int main() {
 	}
 
 	else cout << "Unable to open file";
-
+	
 
 
 	system("Pause");
